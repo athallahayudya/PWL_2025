33af99ab1f9c5e01774edb61f8101d6e6696b035
athallahayudya <athallah08.6a@gmail.com>
2025-03-06 08:03:17 +0700

    migrations

---
+++ Week_3/Jobsheet3/database/migrations/2025_03_05_212710_create_t_stok_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_stok', function (Blueprint $table) {
+            $table->id('stok_id');
+            $table->foreignId('barang_id')->constrained('m_barang')->onDelete('cascade');
+            $table->datetime('stok_tanggal');
+            $table->integer('stok_jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_stok');
+    }
+};
